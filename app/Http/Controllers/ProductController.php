dfafb80b73f4b59d32e51d9d99e4d7fd3a351c2f
Masud99Rana <Masudrana917775@gmail.com>
2019-12-22 11:19:36 +0600

    product crud completed

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -108,6 +108,7 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        return response(null,Response::HTTP_NO_CONTENT);
     }
 }
